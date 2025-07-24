51a665b502f7b667b5052f88b62e07be28a95ce1
antrobot123 <antrobot1234@gmail.com>
2025-07-23 19:21:31 -0700

    Removed ConditionRow definition from main class

--- TransitionConditionSplitterWindow.cs
+++ TransitionConditionSplitterWindow.cs
@@ -310,37 +310,4 @@ public class TransitionConditionSplitterWindow : EditorWindow
         EditorGUI.showMixedValue = false;
         EditorGUILayout.EndHorizontal();
     }
-
-    private class ConditionRow
-    {
-        public AnimatorTransitionBase transition;
-        public int transitionIndex;
-        public int conditionIndex;
-        public AnimatorCondition condition;
-
-        public bool mixedParameter;
-        public bool mixedMode;
-        public bool mixedThreshold;
-
-        public ConditionRow(AnimatorTransitionBase transition, int transitionIndex, int conditionIndex, AnimatorCondition condition)
-        {
-            this.transition = transition;
-            this.transitionIndex = transitionIndex;
-            this.conditionIndex = conditionIndex;
-            this.condition = condition;
-        }
-
-        public ConditionRow(ConditionRow source)
-        {
-            transition = source.transition;
-            transitionIndex = source.transitionIndex;
-            conditionIndex = source.conditionIndex;
-            condition = new AnimatorCondition
-            {
-                parameter = source.condition.parameter,
-                mode = source.condition.mode,
-                threshold = source.condition.threshold
-            };
-        }
-    }
 }
